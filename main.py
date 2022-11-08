5007c69f1022de3025c05b808815259d3de57fe2
SadRus <rystixx@yandex.ru>
2022-11-08 15:52:48 +0300

    Update main.py, .gitignore

--- main.py
+++ main.py
@@ -1,63 +1,51 @@
-import json
 import requests
 import os
-from urllib.parse import urlparse
+#from urllib.parse import urlparse
 from dotenv import load_dotenv
-load_dotenv()
 
 
-def shorten_link(token, url):
-    if 'http:' in url:
-        url = url.replace('http', 'https')
-    response = requests.get(url)
-    response.raise_for_status()
-    headers = {
-        'Authorization': f'Bearer {token}'
-    }
-    data = {
+def shorten_link(url, headers):
+    payload = {
         'long_url': url
     }
-    data = json.dumps(data)
-
     response = requests.post('https://api-ssl.bitly.com/v4/bitlinks',
-                             headers=headers, data=data)
+                             headers=headers, json=payload)
     response.raise_for_status()
-    bitlink = json.loads(response.text)['id']
-    return bitlink
+    return response.json()['id']
 
 
-def count_clicks(token, bitlink):
-    headers = {
-        'Authorization': f'Bearer {token}'
-    }
-    params = {
-        'unit': 'day',
-        'units': '-1'
-    }
+def count_clicks(short_url, headers):
     response = requests.get(
-        f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary',
-        headers=headers, params=params)
+        f'https://api-ssl.bitly.com/v4/bitlinks/{short_url}/clicks/summary',
+        headers=headers)
     response.raise_for_status()
-    return json.loads(response.text)['total_clicks']
+    return response.json()['total_clicks']
 
 
-def is_link_shorten(url):
-    if 'bit.ly' in urlparse(url).path:
+def is_link_shorten(url, headers):
+    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{url}',
+                            headers=headers)
+    if response.ok:
         return True
     return False
 
 
 def main():
-    token = os.environ['TOKEN']
+    token = os.environ['BITLY_TOKEN']
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
     url = input()
+
     try:
-        if is_link_shorten(url):
-            print('Count clicks', count_clicks(token, url))
+        if is_link_shorten(url, headers):
+            print('Count clicks', count_clicks(url, headers))
         else:
-            print('Bitlink', shorten_link(token, url))
-    except requests.exceptions.ConnectionError as error:
+            print('Bitlink', shorten_link(url, headers))
+    except requests.exceptions.HTTPError as error:
         exit(f"Can't get data from server: {error}")
 
 
 if __name__ == '__main__':
+    load_dotenv()
     main()
