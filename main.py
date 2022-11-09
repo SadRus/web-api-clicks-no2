31e850bb85351ce749460020be013c1cfaff1357
SadRus <rystixx@yandex.ru>
2022-11-09 15:47:47 +0300

    add urlparse in functions and other update

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 import os
-#from urllib.parse import urlparse
+from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 
-def shorten_link(url, headers):
+def shorten_link(url, token):
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
     payload = {
         'long_url': url
     }
@@ -14,38 +17,40 @@ def shorten_link(url, headers):
     return response.json()['id']
 
 
-def count_clicks(short_url, headers):
+def count_clicks(parsed_url, token):
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
     response = requests.get(
-        f'https://api-ssl.bitly.com/v4/bitlinks/{short_url}/clicks/summary',
+        f'https://api-ssl.bitly.com/v4/bitlinks/{parsed_url}/clicks/summary',
         headers=headers)
     response.raise_for_status()
     return response.json()['total_clicks']
 
 
-def is_link_shorten(url, headers):
-    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{url}',
-                            headers=headers)
-    if response.ok:
-        return True
-    return False
-
-
-def main():
-    token = os.environ['BITLY_TOKEN']
+def is_link_shorten(parsed_url, token):
     headers = {
         'Authorization': f'Bearer {token}'
     }
-    url = input()
+    response = requests.get(
+        f'https://api-ssl.bitly.com/v4/bitlinks/{parsed_url}',
+        headers=headers)
+    return response.ok
 
+
+def main():
+    load_dotenv()
+    token = os.environ['BITLY_TOKEN']
+    url = input()
+    parsed_url = urlparse(url).netloc + urlparse(url).path
     try:
-        if is_link_shorten(url, headers):
-            print('Count clicks', count_clicks(url, headers))
+        if is_link_shorten(parsed_url, token):
+            print('Count clicks', count_clicks(parsed_url, token))
         else:
-            print('Bitlink', shorten_link(url, headers))
+            print('Bitlink', shorten_link(url, token))
     except requests.exceptions.HTTPError as error:
         exit(f"Can't get data from server: {error}")
 
 
 if __name__ == '__main__':
-    load_dotenv()
     main()
